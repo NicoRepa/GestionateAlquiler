a87f69a5197fd11aba30fb5586d1fd4971024654
NicoRepa <nicolasreparaz@gmail.com>
2025-08-19 17:03:01 -0300

    mejoras en de diseno y  creacion de view 'editar alquiler'

--- apps/alquiler/urls.py
+++ apps/alquiler/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from apps.alquiler.views import AlquilerView, CrearAlquilerView, CrearDuenoView
+from apps.alquiler.views import AlquilerView, CrearAlquilerView, CrearDuenoView, EditarAlquilerView
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
@@ -7,5 +7,6 @@ from . import views
 urlpatterns=[
     path("",AlquilerView.as_view(), name="mis_alquileres"),
     path("crearalquiler/",CrearAlquilerView.as_view(), name="crear_alquiler"),
-    path("creardueno/",CrearDuenoView.as_view(), name="crear_dueno")
+    path("creardueno/",CrearDuenoView.as_view(), name="crear_dueno"),
+    path("editaralquiler/<int:pk>",EditarAlquilerView.as_view(), name="editar_alquiler")
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- apps/alquiler/views.py
+++ apps/alquiler/views.py
@@ -3,7 +3,8 @@ from django.views.generic import TemplateView,CreateView,DeleteView,UpdateView,L
 from apps.alquiler.models import Alquiler, Dueno
 from apps.alquiler.forms import CrearAlquilerForm, CrearDuenoForm
 from django.urls import reverse_lazy
-
+from django.contrib import messages
+from django.shortcuts import Http404, get_object_or_404
 class AlquilerView(ListView):
     model = Alquiler
     template_name = 'mis_alquileres.html'
@@ -26,3 +27,25 @@ class CrearDuenoView(CreateView):
     #template_name = 'crear_dueno.html'
     success_url = reverse_lazy('mis_alquileres') 
 # Create your views here.
+
+class EditarAlquilerView(UpdateView):
+    model = Alquiler
+    form_class = CrearAlquilerForm
+    template_name = 'editar_alquiler.html'
+    success_url = reverse_lazy('mis_alquileres')
+    
+    def get_object(self):
+        try:
+            alquiler = Alquiler.objects.get(pk=self.kwargs['pk'])
+        except Alquiler.DoesNotExist:
+            messages.error(self.request, 'Publicación no encontrada.')
+            raise Http404("Publicación no encontrada")
+        return alquiler
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
