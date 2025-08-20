65b4f19c70ec73f8150d7b88229acd25d516210d
NicoRepa <nicolasreparaz@gmail.com>
2025-08-19 21:50:41 -0300

    mejoras en el js para abrir modals, editar alquiler funcionando

--- static/mis_alquileres.js
+++ static/mis_alquileres.js
@@ -1,24 +1,15 @@
-const modal = document.getElementById('crear_alquiler_modal')
-const btn_abrir_modal = document.getElementById('btn_crear_alquiler_modal')
-const btn_cerrar_modal = document.getElementById('btn_cerrar_modal_crear_alquiler')
-const crear_alquiler_modal = document.getElementById('crear_alquiler_modal')
-btn_abrir_modal.addEventListener('click', () =>{
-    modal.showModal();
-})
+const btn_abrir_alquiler_modal = document.querySelectorAll('.btn_abrir_alquiler_modal')
+const alquiler_modal = document.querySelectorAll('.alquiler_modal')
+const btn_cerrar_modal_alquiler =  document.querySelectorAll('.btn_cerrar_modal_alquiler')
 
-btn_cerrar_modal.addEventListener('click', () =>{
-    modal.close();
-})
+btn_abrir_alquiler_modal.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        alquiler_modal[index].showModal();
+    });
+});
 
-const btn_editar_alquiler_modal = document.getElementById('btn_editar_alquiler_modal')
-const editar_alquiler_modal = document.getElementById('editar_alquiler_modal')
-const btn_cerrar_modal_editar_alquiler =  document.getElementById('btn_cerrar_modal_editar_alquiler')
-
-btn_editar_alquiler_modal.addEventListener('click', () =>{
-    editar_alquiler_modal.showModal();
-})
-
-btn_cerrar_modal_editar_alquiler.addEventListener('click', () =>{
-    editar_alquiler_modal.close();
-})
-
+btn_cerrar_modal_alquiler.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+    alquiler_modal[index].close();
+    });
+});
