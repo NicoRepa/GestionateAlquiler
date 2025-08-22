edc34adb0dfe45c1573b3cf02b129df460466b8a
NicoRepa <nicolasreparaz@gmail.com>
2025-08-22 19:25:49 -0300

    mejoras de estilos y nueva opcion 'metodo de pago'

--- apps/alquiler/admin.py
+++ apps/alquiler/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from .models import Alquiler, Dueno
+from .models import Alquiler, Dueno, MetodoPago
 
 @admin.register(Alquiler)
 class AlquilerPropiedad(admin.ModelAdmin):
-     list_display = ('Direccion_Propiedad','Dueno','Inquilino','Fecha_contrato','Precio_mensual','Meses_actualizacion_IPC','Porcentaje_actualizacion')
+     list_display = ('Direccion_Propiedad','Dueno','Inquilino','Fecha_contrato','Precio_mensual','Meses_actualizacion_IPC','Porcentaje_actualizacion','Cancelacion_pago')
 
 @admin.register(Dueno)
 class AlquilerDueno(admin.ModelAdmin):
      list_display = ('Nombre',)
+
+@admin.register(MetodoPago)
+class MetodoPago(admin.ModelAdmin):
+     list_display = ('Metodo',)
 # Register your models here.
--- apps/alquiler/models.py
+++ apps/alquiler/models.py
@@ -10,10 +10,17 @@ class Alquiler(models.Model):
     Precio_Agua = models.IntegerField(default=0, blank=True)
     Precio_ABL = models.IntegerField(default=0, blank=True)
     Meses_actualizacion_IPC = models.IntegerField(blank=True)
-    Observacion = models.CharField(blank=True)
+    Observacion = models.CharField(blank=True, null=True)
+    Cancelacion_pago = models.ForeignKey('alquiler.MetodoPago', on_delete=models.CASCADE, blank=True, null=True)
     Porcentaje_actualizacion = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True, default=0)
+
 
 class Dueno(models.Model):
     Nombre = models.CharField()
     def __str__(self) -> str:
         return self.Nombre
+    
+class MetodoPago(models.Model):
+    Metodo= models.CharField(max_length=20)
+    def __str__(self) -> str:
+        return self.Metodo
