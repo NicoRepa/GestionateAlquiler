6a2775e2f04d130d2ece69873a9b770dc47dd280
NicoRepa <nicolasreparaz@gmail.com>
2025-09-15 21:20:13 -0300

    estilado de login y registro, icono nuevo

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,5 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class UsuarioManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('El email es un campo obligatorio')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser debe tener is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser debe tener is_superuser=True.')
+        return self.create_user(email, password, **extra_fields)
+
 
 class Usuario(AbstractUser):
     username = None
@@ -7,17 +29,16 @@ class Usuario(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    objects = UsuarioManager()
     groups = models.ManyToManyField(
         'auth.Group',
-        related_name='usuario_set',
+        related_name='custom_user_groups',
         blank=True
     )
     user_permissions = models.ManyToManyField(
         'auth.Permission',
-        related_name='usuario_permissions',
+        related_name='custom_user_permissions',
         blank=True
     )
     def __str__(self):
         return f'{self.first_name} {self.last_name} {self.email}'
-
-# Create your models here.
--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -4,15 +4,29 @@ from django.views.generic import TemplateView, CreateView, UpdateView, DeleteVie
 from django.contrib.auth.views import LoginView
 from .forms import UsuarioRegisterForm, UsuarioLoginForm
 from .models import Usuario
-
+from django.shortcuts import redirect
 class RegistroView(CreateView):
     model = Usuario
     form_class = UsuarioRegisterForm
     template_name = 'register.html'
     success_url = reverse_lazy('mis_alquileres')
+    def dispatch(self, request, *args, **kwargs):
+        # Si el usuario ya está autenticado
+        if request.user.is_authenticated:
+            # Redirecciona a la página de éxito o a donde prefieras
+            return redirect(self.success_url)
+        # Si no, continua con el flujo normal de la vista
+        return super().dispatch(request, *args, **kwargs)
 
 class InicioView(LoginView):
     form_class = UsuarioLoginForm
     template_name = 'login.html'
     success_url = reverse_lazy('mis_alquileres')
+    def dispatch(self, request, *args, **kwargs):
+        # Si el usuario ya está autenticado
+        if request.user.is_authenticated:
+            # Redirecciona a la página de éxito o a donde prefieras
+            return redirect(self.success_url)
+        # Si no, continua con el flujo normal de la vista
+        return super().dispatch(request, *args, **kwargs)
 # Create your views here.
